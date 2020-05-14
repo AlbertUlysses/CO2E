7c2606f7b7a848748c487d0c767283a407540a58
AlbertUlysses <albertulysseschavez@gmail.com>
2020-05-13 22:21:15 -0700

    feat: test should be marked as expected to fail

--- tests/test_beagreenie.py
+++ tests/test_beagreenie.py
@@ -1,4 +1,9 @@
 import pytest
 from beagreenie.beagreenie import fuel_combustion
 
-class TestFuelCombustion(object)
+
+class TestFuelCombustion(object):
+
+    @pytest.mark.xfail
+    def test_fuel_combustion():
+        pass
