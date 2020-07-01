24016b77ce0dd3643fc5abf83835d2e817cb485d
AlbertUlysses <albertulysseschavez@gmail.com>
2020-06-30 17:23:50 -0700

    test: add test

--- tests/test_beagreenie.py
+++ tests/test_beagreenie.py
@@ -4,6 +4,7 @@ from beagreenie.beagreenie import fuel_combustion
 
 class TestFuelCombustion(object):
 
-    @pytest.mark.xfail
-    def test_fuel_combustion():
-        pass
+    def test_fuel_combustion(self):
+        test = fuel_combustion(32)
+        assert test == 275
+
